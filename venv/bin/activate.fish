9cf1a8d46a4c761335f3ec2fe2742285e4db3d1e
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-17 11:27:51 +0530

    made changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/chirag/Downloads/Portfolio/UjalaCygnus/venv"
+set -gx VIRTUAL_ENV "/home/rahul/Desktop/DesktopSept17-2024/Portfolio/UjalaCygnus/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
